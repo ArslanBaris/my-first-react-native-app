6e28a7d6cfc8764f23a658f9834a7d6573d96a68
Barış Arslan <baris.arslan@diatics.com>
2023-09-02 22:59:40 +0300

    first step styles and events

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView,Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>I am learning react-native</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.logo}>./arslan</Text>
+        <Image source={require("./assets/menu.png")} style={styles.menu} />
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.button}>
+          <Text onPress={()=>{console.log("Clicked !!")}} style={{flex:1,alignItems:"center", }}> Click Me</Text>
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    backgroundColor:"red",
+    flex:1
   },
+  header: {
+    height: 80,
+    backgroundColor: "orange",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: 20,
+    paddingRight: 20,
+
+  },
+  logo: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  menu: {
+    width:25,
+    height:25
+
+  },
+  content:{
+    flex:1,
+    backgroundColor:"#fff",
+    alignItems:"center",
+    justifyContent:"center"
+    },
+    button:{
+      width:100,
+      height:40,
+      lineHeight:40,
+      backgroundColor:"darkorange",
+      textAlign:"center",
+      
+    }
 });
